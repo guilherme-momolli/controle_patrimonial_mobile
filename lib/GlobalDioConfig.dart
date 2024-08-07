7e4fb813b586d2790f7f3d15ee59d9e553e2d1bc
Guilherme Momolli <guilherme.momolli@gmail.com>
2024-08-07 09:16:23 -0300

    Criado uma classe de configuração global do DIo

---
+++ lib/GlobalDioConfig.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+
+class GlobalDioConfig {
+  static final Dio _instance = Dio();
+
+  static void configureDio() {
+    _instance.options.baseUrl = 'http://192.168.0.116:8080';
+    _instance.options.headers["Access-Control-Allow-Origin"] = "*";
+    _instance.options.headers["Access-Control-Allow-Credentials"] = true;
+    _instance.options.headers["Access-Control-Allow-Headers"] =
+        "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale";
+    _instance.options.headers["Access-Control-Allow-Methods"] =
+        "GET, HEAD, POST, OPTIONS";
+    //_instance.options.headers['Accept'] = 'application/json';
+  }
+
+  static Dio get instance => _instance;
+}
