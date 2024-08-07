579f2535e45f6f6779e30cf5446d555dc1501825
Guilherme Momolli <guilherme.momolli@gmail.com>
2024-08-07 09:53:03 -0300

    Criação das telas

---
+++ lib/list_hardware.dart
@@ -0,0 +1,114 @@
+import 'package:controle_patrimonial/GlobalDioConfig.dart';
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+class ListarHardwareScreen extends StatefulWidget {
+  @override
+  _ListarHardwareScreenState createState() => _ListarHardwareScreenState();
+}
+
+class _ListarHardwareScreenState extends State<ListarHardwareScreen> {
+  late Dio _dio;
+  List<dynamic> hardwares = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _dio = GlobalDioConfig.instance;
+    _getHardware();
+  }
+
+  Future<void> _deleteHardware(int id) async {
+    try {
+      var response = await _dio.delete('/hardware/delete/$id');
+      if (response.statusCode == 200) {
+        print('Hardware deleted successfully.');
+        _getHardware();
+      } else {
+        print('Failed to delete hardware. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _getHardware() async {
+    try {
+      var response = await _dio.get('/hardware/list');
+      if (response.statusCode == 200) {
+        setState(() {
+          hardwares = response.data;
+        });
+      } else {
+        print('Failed to get hardware. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<List<dynamic>> _fetchHardwares() async {
+    var response = await _dio.get('http://192.168.0.121:8080/hardware/list');
+    return response.data;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Listar Hardwares'),
+      ),
+      body: FutureBuilder<List<dynamic>>(
+        future: _fetchHardwares(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return ListView.builder(
+              itemCount: hardwares.length,
+              itemBuilder: (context, index) {
+                var hardware = hardwares[index];
+                return card(
+                  title: Text(hardware['nome']),
+                  subtitle: Text('id: ${hardware['id']}'),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Editar'),
+                      ),
+                      SizedBox(width: 8),
+                      ElevatedButton(
+                        onPressed: () async {
+                          await _deleteHardware(hardware['id']);
+                        },
+                        child: const Text('Deletar'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  Widget card({
+    required Widget title,
+    required Widget subtitle,
+    required Widget trailing,
+  }) {
+    return Card(
+      child: ListTile(
+        title: title,
+        subtitle: subtitle,
+        trailing: trailing,
+      ),
+    );
+  }
+}
---
+++ lib/update_usuario.dart
@@ -0,0 +1,94 @@
+import 'package:controle_patrimonial/GlobalDioConfig.dart';
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:controle_patrimonial/main.dart';
+
+class UpdateUsuarioScreen extends StatefulWidget {
+  @override
+  _UpdateUsuarioScreenState createState() => _UpdateUsuarioScreenState();
+}
+
+class _UpdateUsuarioScreenState extends State<UpdateUsuarioScreen> {
+  final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _senhaController = TextEditingController();
+  late Dio _dio;
+
+  @override
+  void initState() {
+    super.initState();
+    _dio = GlobalDioConfig.instance;
+  }
+
+  Future<void> _updateUsuario(int id) async {
+    String nome = _nomeController.text;
+    String email = _emailController.text;
+    String senha = _senhaController.text;
+
+    try {
+      var response = await _dio.post(
+        '/usuario/update/$id',
+        data: {
+          'nome': nome,
+          'email': email,
+          'senha': senha,
+        },
+      );
+      if (response.statusCode == 201) {
+        print('Usuario created successfully.');
+      } else {
+        print('Failed to create pessoa. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create Pessoa'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _nomeController,
+              decoration: InputDecoration(
+                labelText: 'Nome',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          
+            SizedBox(height: 16),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: _senhaController,
+              decoration: InputDecoration(
+                labelText: 'Senha',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                //_updateUsuario();
+              },
+              child: Text('Create'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
